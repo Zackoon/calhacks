4a33d9b00380f768cf0afa5265fff3225dda29b8
Nikhil Pitta <nikhil.pitta@berkeley.edu>
2023-01-04 20:46:44 -0800

    deleting files

--- dbinit.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE likedSongs (
-    URI varchar(255) PRIMARY KEY,
-    songName varchar(255)
-);
--- test_db_connection.py
+++
@@ -1,6 +0,0 @@
-import os
-import psycopg2
-user = os.environ["thomasychen"]
-host = os.environ["free-tier4.aws-us-west-2.cockroachlabs.cloud"]
-cluster=os.environ["fading-amoeba-4004"]
-password = os.environ["dM-bok466W3GpAY9pQGXHA"]
